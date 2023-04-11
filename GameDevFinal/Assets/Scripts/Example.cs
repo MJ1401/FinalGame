c25a2a8c4d59c4ded31079ac1a1d57c144b14d8f
MJ1401 <johnsonmb@hendrix.edu>
2023-04-11 10:38:43 -0500

    Save Spot

---
+++ GameDevFinal/Assets/Scripts/Example.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+using UnityEngine.EventSystems; // 1
+ 
+
+ // https://answers.unity.com/questions/1095047/detect-mouse-events-for-ui-canvas.html
+ public class Example : MonoBehaviour
+     , IPointerClickHandler // 2
+     , IDragHandler
+     , IPointerEnterHandler
+     , IPointerExitHandler
+ // ... And many more available!
+ {
+     SpriteRenderer sprite;
+     Color target = Color.red;
+ 
+     void Awake()
+     {
+         sprite = GetComponent<SpriteRenderer>();
+     }
+ 
+     void Update()
+     {
+         if (sprite)
+             sprite.color = Vector4.MoveTowards(sprite.color, target, Time.deltaTime * 10);
+     }
+ 
+     public void OnPointerClick(PointerEventData eventData) // 3
+     {
+         print("I was clicked");
+         target = Color.blue;
+     }
+ 
+     public void OnDrag(PointerEventData eventData)
+     {
+         print("I'm being dragged!");
+         target = Color.magenta;
+     }
+ 
+     public void OnPointerEnter(PointerEventData eventData)
+     {
+         target = Color.green;
+     }
+ 
+     public void OnPointerExit(PointerEventData eventData)
+     {
+         target = Color.red;
+     }
+
+     public void OnMouseUp() {
+        print("Mouse Up Test");
+     }
+ }
