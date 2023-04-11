1e78b398b426e561a8d011d5dc0f26deccf8b6ae
MJ1401 <johnsonmb@hendrix.edu>
2023-04-11 10:46:30 -0500

    Beginning Highlight

--- GameDevFinal/Assets/Scripts/Example.cs
+++ GameDevFinal/Assets/Scripts/Example.cs
@@ -1,52 +1,53 @@
 using UnityEngine;
 using UnityEngine.EventSystems; // 1
+using UnityEngine.UI;
  
 
- // https://answers.unity.com/questions/1095047/detect-mouse-events-for-ui-canvas.html
- public class Example : MonoBehaviour
-     , IPointerClickHandler // 2
-     , IDragHandler
-     , IPointerEnterHandler
-     , IPointerExitHandler
- // ... And many more available!
- {
-     SpriteRenderer sprite;
-     Color target = Color.red;
- 
-     void Awake()
-     {
-         sprite = GetComponent<SpriteRenderer>();
-     }
- 
-     void Update()
-     {
-         if (sprite)
-             sprite.color = Vector4.MoveTowards(sprite.color, target, Time.deltaTime * 10);
-     }
- 
-     public void OnPointerClick(PointerEventData eventData) // 3
-     {
-         print("I was clicked");
-         target = Color.blue;
-     }
- 
-     public void OnDrag(PointerEventData eventData)
-     {
-         print("I'm being dragged!");
-         target = Color.magenta;
-     }
- 
-     public void OnPointerEnter(PointerEventData eventData)
-     {
-         target = Color.green;
-     }
- 
-     public void OnPointerExit(PointerEventData eventData)
-     {
-         target = Color.red;
-     }
+// https://answers.unity.com/questions/1095047/detect-mouse-events-for-ui-canvas.html
+public class Highlighter : MonoBehaviour
+    , IPointerClickHandler // 2
+    , IDragHandler
+    , IPointerEnterHandler
+    , IPointerExitHandler
+// ... And many more available!
+{
+    Image sprite;
+    Color target = Color.red;
 
-     public void OnMouseUp() {
-        print("Mouse Up Test");
-     }
- }
+    void Awake()
+    {
+        sprite = GetComponent<Image>();
+    }
+
+    void Update()
+    {
+        if (sprite)
+            sprite.color = Vector4.MoveTowards(sprite.color, target, Time.deltaTime * 10);
+    }
+
+    public void OnPointerClick(PointerEventData eventData) // 3
+    {
+        print("I was clicked");
+        target = Color.blue;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        print("I'm being dragged!");
+        target = Color.magenta;
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        target = Color.green;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        target = Color.red;
+    }
+
+    public void OnMouseUp() {
+    print("Mouse Up Test");
+    }
+}
--- GameDevFinal/Assets/Scripts/Highlight.cs
+++ GameDevFinal/Assets/Scripts/Highlight.cs
@@ -1,27 +1,53 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems; // 1
 using UnityEngine.UI;
+ 
 
+// https://answers.unity.com/questions/1095047/detect-mouse-events-for-ui-canvas.html
 public class Highlight : MonoBehaviour
+    , IPointerClickHandler // 2
+    , IDragHandler
+    , IPointerEnterHandler
+    , IPointerExitHandler
+// ... And many more available!
 {
+    Image sprite;
+    Color target = Color.red;
 
-    [SerializeField] private Image myImage;
-    [SerializeField] private Color myColor;
-    private Color startcolor;
-
-    private void Start() {
-
-    }
-    
-    public void OnMouseEnter() {
-        print("IM mouse over");
-        startcolor = myImage.color;
-        myImage.color = myColor;
+    void Awake()
+    {
+        sprite = GetComponent<Image>();
     }
 
-    public void OnMouseExit() {
-        print("I'm mouse exit");
-        myImage.color = startcolor;
+    void Update()
+    {
+        if (sprite)
+            sprite.color = Vector4.MoveTowards(sprite.color, target, Time.deltaTime * 10);
+    }
+
+    public void OnPointerClick(PointerEventData eventData) // 3
+    {
+        print("I was clicked");
+        target = Color.blue;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        print("I'm being dragged!");
+        target = Color.magenta;
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        target = Color.green;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        target = Color.red;
+    }
+
+    public void OnMouseUp() {
+    print("Mouse Up Test");
     }
 }
