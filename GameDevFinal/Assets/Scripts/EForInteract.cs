c35751f1e72bdc7a4a6a912c0ed6054d771f1c72
Tucker Brock <brocktk@hendrix.edu>
2023-04-11 07:43:37 -0500

    E pops up text
    Now need it to close it and collect evidence

--- GameDevFinal/Assets/Scripts/EForInteract.cs
+++ GameDevFinal/Assets/Scripts/EForInteract.cs
@@ -1,64 +1,100 @@
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+// using UnityEngine.UI;
+
+// public class EForInteract : MonoBehaviour {
+
+//     public string text;
+
+//     private bool canCollectClue;
+
+//     public string evidence_name;
+
+//     private bool dialogShown = false;
+
+//     public void OnTriggerEnter2D(Collider2D collider2D) {
+//         print("Entered..");
+//         print("Press E to Interact.");
+//         if (Input.GetKeyDown(KeyCode.E) || Input.GetKeyDown(KeyCode.Return)) {
+//             if (collider2D.gameObject.CompareTag("Player")) {
+//                 // GameManager.Instance.DialogShow(text);
+//                 canCollectClue = true;
+//             }
+//         }
+//     }
+
+//     public void OnTriggerExit2D(Collider2D collider2D) {
+//         if (collider2D.gameObject.CompareTag("Player"))
+//         {
+//             // GameManager.Instance.DialogHide();
+//             canCollectClue = false;
+//         }
+//     }
+
+//     void Start() {
+
+//     }
+
+//     void Update() {
+//         if (canCollectClue && Input.GetKeyDown(KeyCode.E))
+//         {
+//             if (!dialogShown)
+//             {
+//                 GameManager.Instance.DialogShow(text);
+//                 dialogShown = true;
+//             }
+//             else
+//             {
+//                 collect();
+//                 GameManager.Instance.DialogHide();
+//                 dialogShown = false;
+//             }
+//         }
+//     }
+
+//     private void collect() {
+//         // set text to something like "can I help you? and can repeat"
+//         // add evidence to collection
+//         GameManager.AddEvidence(evidence_name);
+//         print("Evidence collected");
+//     }
+
+
+// }
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class EForInteract : MonoBehaviour {
 
     public string text;
 
-    private bool canCollectClue;
-
-    public string evidence_name;
-
-    private bool dialogShown = false;
+    private bool canShowDialog;
 
     public void OnTriggerEnter2D(Collider2D collider2D) {
-        print("Entered..");
-        print("Press E to Interact.");
-        if (Input.GetKeyDown(KeyCode.E) || Input.GetKeyDown(KeyCode.Return)) {
-            if (collider2D.gameObject.CompareTag("Player")) {
-                // GameManager.Instance.DialogShow(text);
-                canCollectClue = true;
-            }
+        if (collider2D.gameObject.CompareTag("Player")) {
+            canShowDialog = true;
         }
     }
 
     public void OnTriggerExit2D(Collider2D collider2D) {
         if (collider2D.gameObject.CompareTag("Player"))
         {
-            // GameManager.Instance.DialogHide();
-            canCollectClue = false;
+            GameManager.Instance.DialogHide();
+            canShowDialog = false;
         }
-    }
-
-    void Start() {
-
     }
 
     void Update() {
-        if (canCollectClue && Input.GetKeyDown(KeyCode.E))
+        if (canShowDialog && Input.GetKeyDown(KeyCode.E))
         {
-            if (!dialogShown)
-            {
-                GameManager.Instance.DialogShow(text);
-                dialogShown = true;
-            }
-            else
-            {
-                collect();
-                GameManager.Instance.DialogHide();
-                dialogShown = false;
-            }
+            GameManager.Instance.DialogShow(text);
+        }
+        else if (Input.GetKeyDown(KeyCode.E))
+        {
+            GameManager.Instance.DialogHide();
         }
     }
-
-    private void collect() {
-        // set text to something like "can I help you? and can repeat"
-        // add evidence to collection
-        GameManager.AddEvidence(evidence_name);
-        print("Evidence collected");
-    }
-
-
 }
