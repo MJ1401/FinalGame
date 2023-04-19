47778ded9064aa619c54acd130d98d68aeed4a73
Gabby <gnaples19@gmail.com>
2023-04-18 21:30:43 -0500

    Cannot get the sound effects to work on title

--- GameDevFinal/Assets/Scripts/Button.cs
+++ GameDevFinal/Assets/Scripts/Button.cs
@@ -5,13 +5,16 @@ using UnityEngine;
 public class Button : MonoBehaviour
 {
     public string scene;
+    private AudioSource audio;
+
     public void ToScene(){
         GameManager.Instance.ChangeScene(scene);
     }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        audio = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
